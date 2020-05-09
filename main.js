f3830b3cd446a3398aa61f7482f2eb233d3f55a1
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-05-09 18:34:25 -0400

    included all five add events

--- main.js
+++ main.js
@@ -1,19 +1,56 @@
 const starWars = 'https://swapi.dev/api/people'
 
-fetch(starWars)
-	.then(res => {
-		return res.json()
-	})
-	.then(res => {
-		const list = document.createElement('ul')
-		res.results.forEach(function(character){
-			const listItem = document.createElement('li')
-			listItem.textContent = character.name
-			list.appendChild(listItem)
+let section1 = document.querySelector('.section1')
+section1.addEventListener('click', function(e){
+	console.log(e.target)
+	fetch(starWars)
+		.then(res => {
+			return res.json()
 		})
+		.then(res => {
+			const list = document.createElement('ul')
+			res.results.forEach(function(character){
+				const listItem = document.createElement('li')
+				listItem.textContent = character.name
+				list.appendChild(listItem)
+			})
 
-		document.querySelector('div').appendChild(list)
-	})
-	.catch(error => {
-		console.log("there has been a disturbance in the force")
-	})
+			document.querySelector('div').appendChild(list)
+		})
+		.catch(error => {
+			console.log("there has been a disturbance in the force")
+		})
+})
+
+
+let section2 = document.querySelector('.section2')
+section2.addEventListener('click', function(e){
+
+
+})
+
+
+let section3 = document.querySelector('.section3')
+section3.addEventListener('click', function(e){
+
+	
+})
+
+
+let section3 = document.querySelector('.section3')
+section3.addEventListener('click', function(e){
+
+	
+})
+
+let section4 = document.querySelector('.section4')
+section4.addEventListener('click', function(e){
+
+	
+})
+
+let section5 = document.querySelector('.section5')
+section5.addEventListener('click', function(e){
+
+	
+})
