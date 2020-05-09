46baad3914d554fa9d2a6bc542f618c4e0db4219
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-05-09 18:15:44 -0400

    right now it is producing results

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("hello")
+const starWars = 'https://swapi.dev/api/people'
+
+fetch(starWars)
+	.then(res => {
+		return res.json()
+	})
+	.then(res => {
+		const list = document.createElement('ul')
+		res.results.forEach(function(character){
+			const listItem = document.createElement('li')
+			listItem.textContent = character.name
+			list.appendChild(listItem)
+		})
+
+		document.querySelector('div').appendChild(list)
+	})
+	.catch(error => {
+		console.log("there has been a disturbance in the force")
+	})
