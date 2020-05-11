219fcfc7e9a7cbbfbbf00609f3fd566766ce3417
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-05-11 18:35:50 -0400

    need to make a loop

--- main2.js
+++ main2.js
@@ -7,15 +7,31 @@ fetch(starWarsApiUrl)
 		data.results.forEach(character => {
 			let listItem = document.createElement('li')
 			listItem.textContent = character.name
+			let container = document.createElement('div')
+			container.classList.add('hidden')
+			container.classList.add('loop')
 			let height = document.createElement('p')
-			height.textContent = character.height
-			height.style.display = 'none'
-			listItem.appendChild(height)
+			height.textContent = 'height: ' + character.height
+			let mass = document.createElement('p')
+			mass.textContent = 'mass: ' + character.mass
+			let gender = document.createElement('p')
+			gender.textContent = 'gender: ' + character.gender
+			let birth = document.createElement('p')
+			birth.textContent = 'year of birth: ' + character.birth_year
+			listItem.appendChild(container)
+			container.appendChild(birth)
+			container.appendChild(height)
+			container.appendChild(mass)
+			container.appendChild(gender)
 			unorderedList.appendChild(listItem)
+			
+
 
 			listItem.addEventListener('click', () => {
-				height.style.display = 'block'
-				if ()
+				container.classList.toggle('hidden')
+				for (let i = 0; i <= 10; i++) {
+					
+				}
 			})
 		})
 	})
